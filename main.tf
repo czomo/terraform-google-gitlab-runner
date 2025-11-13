e7ec7ede1cd17f933550f9a539acac609b502aaf
Tomasz <35335044+czomo@users.noreply.github.com>
2025-11-13 08:20:26 +0100

    chore: NO-ISSUE max_unavailable_percent

--- main.tf
+++ main.tf
@@ -224,7 +224,7 @@ resource "google_compute_instance_group_manager" "gitlab_runner_mig" {
     type                         = "OPPORTUNISTIC"
     minimal_action               = "REPLACE"
     max_surge_fixed              = 0
-    max_unavailable_fixed        = 0
+    max_unavailable_percent      = 50
     replacement_method           = "SUBSTITUTE"
   }
 
